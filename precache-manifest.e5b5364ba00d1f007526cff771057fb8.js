002618f881533a4a106f4222c4874357ea53dc70
Prathap09 <prathap4139@gmail.com>
2019-11-30 11:17:12 +0530

    Updates

---
+++ precache-manifest.e5b5364ba00d1f007526cff771057fb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ab41deafceb203d122d61b05fc6e55",
+    "url": "/montsters-shit/index.html"
+  },
+  {
+    "revision": "52da6aae4223ba345f46",
+    "url": "/montsters-shit/static/css/main.8739ec78.chunk.css"
+  },
+  {
+    "revision": "3daa2bdc65da714b4181",
+    "url": "/montsters-shit/static/js/2.e2ce35a1.chunk.js"
+  },
+  {
+    "revision": "52da6aae4223ba345f46",
+    "url": "/montsters-shit/static/js/main.c6a73a0a.chunk.js"
+  },
+  {
+    "revision": "61d29df4ab22df54c77b",
+    "url": "/montsters-shit/static/js/runtime-main.94c76e3d.js"
+  }
+]);
